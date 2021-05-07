d9572d6f1bcff8a253c7d2804ee918326cabadf2
Sergio Cano <li_sergiocm@unca.edu.mx>
2021-05-07 00:05:46 -0500

    archivos del curso

--- index.js
+++ index.js
@@ -7,30 +7,54 @@ console.log(os.platform());
 console.log(os.release());
 console.log("Free mem: "+ os.freemem());
 console.log("Total Memory: "+ os.totalmem());
+
 const fs = require('fs');
+
 fs.writeFile('./texto.txt', 'linea uno', function(err){
     if(err){
         console.log(err);
     }
     console.log('Archivo Creado');
 });
+
 console.log('Ultima linea de codigo');
+
 fs.readFile('./texto.txt', function(err, data){
     if(err){
         console.log(err);
     }
     console.log(data.toString());
 });
-const http = require('http');
+
+/* const http = require('http');
+
 const handleServer = function (req, res) {
     res.writeHead(200, {'Content-type':'text/html'});
     res.write('<h1>Hola mundo desde NodeJS</h1>');
     res.end();
 }
+
 const server = http.createServer(handleServer);
+
 server.listen(3000, function(){
     console.log('Server on port 3000');
+}) */
+
+/* o  con Express JS*/
+
+const express = require('express');
+const colors = require('color');
+const server = express();
+
+server.get('/', (req, res) =>{
+    res.send('<h1>Hola mundo desde Node JS y Express JS</h1>');
+    res.end();
 })
+
+server.listen(3000, ()=>{
+    console.log('Server on port 3000'.red);
+})
+
 
 /* console.log("El resultado de la suma entre "+num1+" y "+num2+" es: "+math.suma(num1,num2));
 console.log("El resultado de la resta entre "+num1+" y "+num2+" es: "+math.resta(num1,num2));
